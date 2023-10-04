76527425674654a272875f0e8e067d98343d1f88
Carmen <cmardom599@g.educaand.es>
2023-10-04 14:07:02 +0200

    ListaOrdenadaTest: todos los int comprobados, faltan String

--- src/test/java/Test/ListaOrdenadaTest.java
+++ src/test/java/Test/ListaOrdenadaTest.java
@@ -7,6 +7,56 @@ import org.junit.jupiter.api.Test;
 
 class ListaOrdenadaTest {
     @Test
+    public void addTest(){
+        ListaOrdenada<Integer> listaOrdenada;
+        //Creamos lista y añadimos integers
+        listaOrdenada = new ListaOrdenada<>();
+        listaOrdenada.add(123);
+        listaOrdenada.add(1);
+        listaOrdenada.add(221);
+        listaOrdenada.add(8);
+
+        Assertions.assertEquals(listaOrdenada.get(0), 1);
+        Assertions.assertNotEquals(listaOrdenada.get(0), 123);
+
+    }
+
+    @Test
+    public void getTest(){
+        ListaOrdenada<Integer> listaOrdenada;
+        //Creamos lista y añadimos integers
+        listaOrdenada = new ListaOrdenada<>();
+        listaOrdenada.add(15);
+        listaOrdenada.add(221);
+        listaOrdenada.add(8);
+
+
+        Assertions.assertEquals(15, listaOrdenada.get(0));
+    }
+
+    @Test
+    public void sizeTest(){
+        ListaOrdenada<Integer> listaOrdenada;
+        //Creamos lista y añadimos integers
+        listaOrdenada = new ListaOrdenada<>();
+        listaOrdenada.add(15);
+        listaOrdenada.add(221);
+        listaOrdenada.add(8);
+
+        Assertions.assertEquals(3, listaOrdenada.size());
+    }
+
+    @Test
+    public void isEmptyTest(){
+        ListaOrdenada<Integer> listaOrdenada;
+        //Creamos lista y añadimos integers
+        listaOrdenada = new ListaOrdenada<>();
+
+        Assertions.assertEquals(true, listaOrdenada.isEmpty());
+    }
+
+
+    @Test
     public void removeTest(){
         ListaOrdenada<Integer> listaOrdenada;
         //Creamos lista y añadimos integers
@@ -22,19 +72,30 @@ class ListaOrdenadaTest {
     }
 
     @Test
-    public void addTest(){
+    public void indexOfTest(){
         ListaOrdenada<Integer> listaOrdenada;
         //Creamos lista y añadimos integers
         listaOrdenada = new ListaOrdenada<>();
-        listaOrdenada.add(123);
         listaOrdenada.add(1);
-        listaOrdenada.add(221);
-        listaOrdenada.add(8);
+        listaOrdenada.add(123);
 
-        Assertions.assertEquals(listaOrdenada.get(0), 1);
-        Assertions.assertNotEquals(listaOrdenada.get(0), 123);
-
+        Assertions.assertEquals(1, listaOrdenada.indexOf(123));
     }
+
+    @Test
+    public void toStringTest(){
+        ListaOrdenada<Integer> listaOrdenada;
+        //Creamos lista y añadimos integers
+        listaOrdenada = new ListaOrdenada<>();
+        listaOrdenada.add(555481);
+
+        String expected = "ListaOrdenada{" +
+                "elementos=[" + listaOrdenada.get(0) +
+                "]}";
+
+        Assertions.assertEquals(expected, listaOrdenada.toString());
+    }
+
 
 
 
